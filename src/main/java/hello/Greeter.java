ffa4c27150684af386a3955cdd498f5cd5b940d9
pradip-1137 <96612151+pradip-1137@users.noreply.github.com>
2024-03-13 11:58:40 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Welcome to Unnati Development jenkin test..... Bye";
+    return "Welcome to Unnati Development JENKINS TEST2 after change in GitHub..... Bye";
   }
 }
